3b6a79aa38d29f9a27e243dac0790e4fa0ec18b7
scacyba <idtrmnt@gmail.com>
2026-06-03 15:53:58 +0900

    Update build.gradle.kts

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     signingConfigs {
         if (hasReleaseKeystore) {
             create("release") {
-                storeFile = file(keystoreProperties["storeFile"] as String)
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
                 storePassword = keystoreProperties["storePassword"] as String
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
